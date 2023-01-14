7e3287cb8e30cecde997ef66ef35ffb30230a22c
xenbyte <h.behbahanian@digikala.com>
2023-01-15 00:57:19 +0330

    Day 3 - second star with tests

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -4,10 +4,12 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+use itertools::Itertools;
+
 fn main() {
     let file = File::open("./input/input.txt").expect("Could not fine the file");
     let reader = BufReader::new(file);
-    let score = rucksack_get_sum(reader, 1);
+    let score = rucksack_get_sum(reader, 2);
     println!("Final Score is {}", score);
 }
 
@@ -33,7 +35,21 @@ fn rucksack_get_sum<R: BufRead>(rucksacks: R, round: i32) -> i32 {
 
             return score;
         }
-        2 => (),
+        2 => {
+            let mut score: i32 = 0;
+            for (x, y, z) in rucksacks.lines().tuples() {
+                println!("THIS");
+                let common_str = find_common_str(
+                    &mut score,
+                    x.expect("Not Found"),
+                    y.expect("Not Found"),
+                    z.expect("Not Found"),
+                );
+                update_score(&mut score, common_str);
+
+            }
+            return score;
+        }
         _ => (),
     }
     10
@@ -53,9 +69,39 @@ fn update_score(score: &mut i32, common_str: char) {
     }
 }
 
+fn find_common_str(score: &mut i32, x: String, y: String, z: String) -> char {
+    let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .collect();
+    let mut common_char: char = '0';
+    x.chars().any(|c| {
+        if y.contains(c) && z.contains(c) {
+            common_char = c;
+        }
+        false
+    });
+    return common_char;
+}
+
 // fn rucksack_get_share_char(first_rucksack: &str, second_rucksack: &str) ->
 #[cfg(test)]
 mod tests {
+    use std::{fs::File, io::BufReader};
+
+    use crate::rucksack_get_sum;
+
     #[test]
-    fn puzzle_1() {}
+    fn puzzle_1() {
+        let file = File::open("./input/test_input.txt").expect("Could not fine the file");
+        let reader = BufReader::new(file);
+        let score = rucksack_get_sum(reader, 1);
+        assert_eq!(score, 157);
+    }
+    #[test]
+    fn puzzle_2() {
+        let file = File::open("./input/test_input.txt").expect("Could not fine the file");
+        let reader = BufReader::new(file);
+        let score = rucksack_get_sum(reader, 2);
+        assert_eq!(score, 70);
+    }
 }
